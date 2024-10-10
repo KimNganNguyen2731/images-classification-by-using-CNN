1152292e2bef8632e126dcc859515d012f8712b9
KimNganNguyen <nkngan2731@gmail.com>
2024-10-10 23:27:32 +0700

    Update plots.py

--- utils/plots.py
+++ utils/plots.py
@@ -1,1 +1,10 @@
+import matplotlib.pyplot as plt
 
+
+def loss_curve(loss_result):
+  plt.plot(loss_result, color = 'red')
+  plt.xlabel('Step')
+  plt.ylabel('Loss')
+  plt.title('Loss Curve')
+  plt.savefig('loss_curve.png')
+  #plt.show()
