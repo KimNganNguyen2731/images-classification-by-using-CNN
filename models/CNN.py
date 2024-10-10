638c2c7e8283fda2a8b8535beaf97b9a5f965a4c
KimNganNguyen <nkngan2731@gmail.com>
2024-10-10 20:54:17 +0700

    Update CNN.py

--- models/CNN.py
+++ models/CNN.py
@@ -1,1 +1,13 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+
+# Convolutional Neural Network
+# 1. Filter
+# 2. Max Pooling
+# 3. Fully Connected Layer
+
+# Device
+DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
