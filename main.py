0e8fa5b3b8ab5401f7370fd03cea3bc522393052
KimNganNguyen <nkngan2731@gmail.com>
2024-10-10 23:34:57 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
-from data. import *
-from model.CNN import ConvNet
+from data.CIFAR10 import dataloader
+from models.CNN import ConvNet
 from utils.utils import train, test
+
+def main():
+  train_loader, test_loader = dataloader()
+  trained_model = train()
+  test()
